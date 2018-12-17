dc6f5626b021e4124ec8b491a4a1da73c15fbee0
lakmal silva <lakmals@ellipsis.lk>
2018-12-17 14:04:43 +0530

    Task 1

--- Test/Controllers/HomeController.cs
+++ Test/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Test.Models;
 
 namespace Test.Controllers
 {
@@ -10,6 +11,11 @@ namespace Test.Controllers
     {
         public ActionResult Index()
         {
+            // Task 1
+            ViewBag.q1Basic1 = LoanCalculator.GetMonthlyRepayment(0.9 * 500000, 4.65, 30);
+            ViewBag.q1Advance1 = LoanCalculator.GetNumberOfYears(0.9 * 500000, 4.65, 2000);
+            ViewBag.q1Advance2 = LoanCalculator.CheckYearsAndGetLoanAmount(0.9 * 500000, 4.65, 2000, 35, 30);
+
             return View();
         }
     }
---
+++ Test/Models/LoanCalculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class LoanCalculator
+    {
+        public static double GetMonthlyRepayment(double principleAmount, double percentageRatePerYear, 
+            int numberOfYears)
+        {
+            var rate = percentageRatePerYear / (100 * 12);
+            var numberOfPayments = numberOfYears * 12;
+            var payment = principleAmount * (rate * Math.Pow(1 + rate, numberOfPayments)) / 
+                          (Math.Pow(1 + rate, numberOfPayments) - 1);
+            return Math.Round(payment, 2);
+        }
+
+        public static int GetNumberOfYears(double principleAmount, double percentageRatePerYear, double payment)
+        {
+            var rate = percentageRatePerYear / (100 * 12);
+            var numberOfYears = Math.Log10(payment / (payment - principleAmount * rate)) / Math.Log10(1 + rate);
+            return (int) numberOfYears / 12;
+        }
+
+        private static double GetLoanAmount(double percentageRatePerYear, double payment, int numberOfYears)
+        {
+            var rate = percentageRatePerYear / (100 * 12);
+            var numberOfPayments = numberOfYears * 12;
+            var amount = payment * (Math.Pow(1 + rate, numberOfPayments) - 1) /
+                         (rate * Math.Pow(1 + rate, numberOfPayments));
+            return Math.Round(amount, 2);
+        }
+
+        /*
+         * If loan period exceeds the maxNumberOfYears loan amount will recalculate using
+         * the numberOfYearsForRecalculate
+         */
+        public static double CheckYearsAndGetLoanAmount(double principleAmount, double percentageRatePerYear,
+            double payment, int maxNumberOfYears, int numberOfYearsForRecalculate)
+        {
+            if (GetNumberOfYears(principleAmount, percentageRatePerYear, payment) > maxNumberOfYears)
+            {
+                return GetLoanAmount(percentageRatePerYear, payment, numberOfYearsForRecalculate);
+            }
+            else
+            {
+                return principleAmount;
+            }
+        }
+    }
+}
